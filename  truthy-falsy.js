a4289245f9947e555035b7f39e63eea91323be47
jeisyane <160151590+jeisyane@users.noreply.github.com>
2024-11-22 12:21:58 -0300

    carga_js-tr-falsy

---
+++  truthy-falsy.js
@@ -0,0 +1,34 @@
+const x = 0;
+
+//boolean
+const usuarioLogado=true;
+const contaPaga=false
+
+//truthy ou falsy
+//0-> false
+//1->true
+
+console.log (0==false)//retorna true
+console.log(""==false)//retora true
+console.log(1==true)
+
+console.log("null e undefinned")
+//undefined
+//null-> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log("quando o unfined esta em sua forma pura")
+
+let numero=3;
+let texto="alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
